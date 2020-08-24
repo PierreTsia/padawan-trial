c51ca3d8787084a6857cb98a8ad880a2bd630209
Pierre_T <pierre.tsiakkaros@gmail.com>
2020-08-24 21:15:42 +0200

    :heavy_plus_sign: :boom: add composition api plugin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
+<script lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from "@vue/composition-api";
+const Home = defineComponent({
   name: 'Home',
-  components: {
+  components:{
     HelloWorld
   }
-}
+});
+
+export default Home;
+
+
 </script>
