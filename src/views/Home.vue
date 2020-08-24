39d5f56c9b626bbb896269c85ee36dc7769bcd03
Pierre_T <pierre.tsiakkaros@gmail.com>
2020-08-24 21:20:57 +0200

    :heavy_plus_sign: :wrench: add eslint-prettier plugin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -8,13 +8,11 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 import { defineComponent } from "@vue/composition-api";
 const Home = defineComponent({
-  name: 'Home',
-  components:{
+  name: "Home",
+  components: {
     HelloWorld
   }
 });
 
 export default Home;
-
-
 </script>
